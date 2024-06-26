6f154c03d583a33350155896dbf2965fa4dca0a5
zahidprvz <pervaizzahid55@gmail.com>
2024-06-26 09:36:40 +0500

    updated ip address text field

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,77 +14,85 @@ class HomeScreen extends StatelessWidget {
     return BaseScreen(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter IP Address',
-                hintStyle: TextStyle(color: isDarkMode ? AppTheme.secondaryColor : null),
-                labelText: 'IP Address',
-                labelStyle: TextStyle(color: isDarkMode ? AppTheme.secondaryColor : null),
-                border: const OutlineInputBorder(),
-                filled: true,
-                fillColor: isDarkMode ? theme.primaryColor.withOpacity(0.1) : Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 12.0),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter IP Address',
+                  hintStyle: TextStyle(
+                      color: isDarkMode ? AppTheme.secondaryColor : null),
+                  labelText: 'IP Address',
+                  labelStyle: TextStyle(
+                      color: isDarkMode ? AppTheme.secondaryColor : null),
+                  border: const OutlineInputBorder(),
+                  filled: true,
+                  fillColor: isDarkMode
+                      ? theme.primaryColor.withOpacity(0.1)
+                      : Colors.white,
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: isDarkMode ? Colors.white : null),
               ),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: isDarkMode ? theme.primaryColor : null),
-            ),
-            const SizedBox(height: 24.0),
-            Center(
-              child: Column(
+              const SizedBox(height: 24.0),
+              Center(
+                child: Column(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_upward),
+                      iconSize: 48.0,
+                      color: theme.colorScheme.secondary,
+                      onPressed: () {},
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          iconSize: 48.0,
+                          color: theme.colorScheme.secondary,
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 48.0),
+                        IconButton(
+                          icon: const Icon(Icons.arrow_forward),
+                          iconSize: 48.0,
+                          color: theme.colorScheme.secondary,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 48.0,
+                      color: theme.colorScheme.secondary,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              Text(
+                'Gyro Values:',
+                style: TextStyle(
+                    color: isDarkMode ? AppTheme.secondaryColor : null),
+              ),
+              const SizedBox(height: 8.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_upward),
-                    iconSize: 48.0,
-                    color: theme.colorScheme.secondary,
-                    onPressed: () {},
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        iconSize: 48.0,
-                        color: theme.colorScheme.secondary,
-                        onPressed: () {},
-                      ),
-                      const SizedBox(width: 48.0),
-                      IconButton(
-                        icon: const Icon(Icons.arrow_forward),
-                        iconSize: 48.0,
-                        color: theme.colorScheme.secondary,
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 48.0,
-                    color: theme.colorScheme.secondary,
-                    onPressed: () {},
-                  ),
+                  GyroValueBox(
+                      title: 'X', value: '0.00', isDarkMode: isDarkMode),
+                  GyroValueBox(
+                      title: 'Y', value: '0.00', isDarkMode: isDarkMode),
+                  GyroValueBox(
+                      title: 'Z', value: '0.00', isDarkMode: isDarkMode),
                 ],
               ),
-            ),
-            const SizedBox(height: 24.0),
-            Text(
-              'Gyro Values:',
-              style: TextStyle(
-                color: isDarkMode ? AppTheme.secondaryColor : null
-              ),
-            ),
-
-            const SizedBox(height: 8.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                GyroValueBox(title: 'X', value: '0.00', isDarkMode: isDarkMode),
-                GyroValueBox(title: 'Y', value: '0.00', isDarkMode: isDarkMode),
-                GyroValueBox(title: 'Z', value: '0.00', isDarkMode: isDarkMode),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       selectedIndex: 0,
@@ -110,7 +118,8 @@ class GyroValueBox extends StatelessWidget {
   final String value;
   final bool isDarkMode;
 
-  const GyroValueBox({required this.title, required this.value, required this.isDarkMode});
+  const GyroValueBox(
+      {required this.title, required this.value, required this.isDarkMode});
 
   @override
   Widget build(BuildContext context) {
@@ -127,12 +136,21 @@ class GyroValueBox extends StatelessWidget {
         children: [
           Text(
             title,
-            style: TextStyle(fontSize: 16.0, color: isDarkMode ? AppTheme.secondaryColor : AppTheme.primaryColor ),
+            style: TextStyle(
+                fontSize: 16.0,
+                color: isDarkMode
+                    ? AppTheme.secondaryColor
+                    : AppTheme.primaryColor),
           ),
           const SizedBox(height: 4.0),
           Text(
             value,
-            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: isDarkMode ? AppTheme.secondaryColor : AppTheme.primaryColor),
+            style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                color: isDarkMode
+                    ? AppTheme.secondaryColor
+                    : AppTheme.primaryColor),
           ),
         ],
       ),
